8f7bc4b41d523868ccf8783763fe4453bddc3174
zack9433 <zack9433@gmail.com>
2015-10-23 01:41:41 +0800

    refactor(i18n): add i18n

--- app/app.js
+++ app/app.js
@@ -8,3 +8,16 @@ let app = angular.module('webapp', [component]);
 app.config(restProvider => {
   restProvider.configure({basePath: 'http://private-d8e84-sanjigeneric.apiary-mock.com'});
 });
+
+class AppController {
+  constructor($translate, LANG_KEYS) {
+    this.$translate = $translate;
+    this.currentLang = $translate.use();
+    this.langs = LANG_KEYS;
+  }
+
+  changeLang(lang) {
+    this.$translate.use(lang);
+  }
+}
+app.controller('AppController', AppController);
