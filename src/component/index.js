de198385ba61fe76a7529fe37b134ec6dcb831b2
kmsheng <kmsh3ng@gmail.com>
2018-05-20 18:23:45 +0800

    style: renamed window.component.js -> ethernet-window.component.js

--- src/component/index.js
+++ src/component/index.js
@@ -6,7 +6,7 @@ import { sjEthernetForm } from './form';
 import i18nConfig from './component.i18n';
 import { EthernetAction, ethernets, GET_ETHERNETS, UPDATE_ETHERNET } from './component.state';
 import EthernetService from './component.service';
-import EthernetWindowComponent from './window.component';
+import EthernetWindowComponent from './ethernet-window.component';
 
 const sjEthernet = angular
   .module('sanji.ethernet', [sjEthernetInfo, sjEthernetForm])
