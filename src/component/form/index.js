56697c37e15d43e5496707f70038db5858b0a105
kmsheng <kmsh3ng@gmail.com>
2018-05-21 14:29:44 +0800

    style: rename container.component.js -> ethernet-form.container.js

--- src/component/form/index.js
+++ src/component/form/index.js
@@ -1,11 +1,11 @@
 import angular from 'angular';
 import './sanji-ethernet-form.tpl.html';
-import EthernetFormContainerComponent from './container.component';
+import EthernetFormContainer from './ethernet-form.container';
 import EthernetFormComponent from './form.component';
 
 const sjEthernetForm = angular
   .module('sanji.ethernet.form', [])
-  .component('sanjiEthernetFormContainer', EthernetFormContainerComponent)
+  .component('sanjiEthernetFormContainer', EthernetFormContainer)
   .component('sanjiEthernetForm', EthernetFormComponent).name;
 
 export { sjEthernetForm };
