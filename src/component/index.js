d2ee26ea7f1a7eee7485416a100e0cfe4e9ae263
kmsheng <kmsh3ng@gmail.com>
2018-05-22 15:22:30 +0800

    refactor: delete info folder

--- src/component/index.js
+++ src/component/index.js
@@ -1,5 +1,4 @@
 import angular from 'angular';
-import { sjEthernetInfo } from './info';
 import { sjEthernetForm } from './form';
 import i18nConfig from './component.i18n';
 import { EthernetAction, ethernets, GET_ETHERNETS, UPDATE_ETHERNET } from './component.module';
@@ -7,7 +6,7 @@ import EthernetService from './ethernet.service';
 import EthernetWindowComponent from './ethernet-window.component';
 
 const sjEthernet = angular
-  .module('sanji.ethernet', [sjEthernetInfo, sjEthernetForm])
+  .module('sanji.ethernet', [sjEthernetForm])
   .config(i18nConfig)
   .factory('ethernetAction', EthernetAction)
   .service('ethernetService', EthernetService)
--- src/component/info/ethernet-info.container.js
+++
@@ -1,7 +0,0 @@
-import EthernetInfoContainerController from './ethernet-info-container.controller';
-
-const EthernetInfoContainer = {
-  template: `<sanji-ethernet-info data="$ctrl.data" tab-index="$ctrl.sanjiWindowMgr.tabIndex"></sanji-ethernet-info>`,
-  controller: EthernetInfoContainerController
-};
-export default EthernetInfoContainer;
